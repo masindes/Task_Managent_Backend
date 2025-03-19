ab6ac247b27b1cc085dc0436c973b7054dd1d0ab
Watty_Sylvester <88721196+masindes@users.noreply.github.com>
2025-03-19 23:06:49 +0300

    changed models

--- models.py
+++ models.py
@@ -1,39 +1,53 @@
 from datetime import datetime
 from enum import Enum
-from flask_sqlalchemy import SQLAlchemy
-from app import User
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
-db = SQLAlchemy()
 
-# Enum for task status
 class TaskStatus(Enum):
     PENDING = "Pending"
     IN_PROGRESS = "In Progress"
     COMPLETED = "Completed"
 
-# User model
+
+class UserRole(Enum):
+    ADMIN = "Admin"
+    USER = "User"
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    tasks = db.relationship('Task', backref='user', lazy=True) 
+    name = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)  
+    role = db.Column(db.Enum(UserRole), nullable=False, default=UserRole.USER) 
+    tasks = db.relationship('Task', backref='user', lazy=True)  
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def to_dict(self):
         return {
             "id": self.id,
             "username": self.username,
             "email": self.email,
-            "tasks": [task.to_dict() for task in self.tasks]  
+            "name": self.name,
+            "role": self.role.value,
+            "tasks": [task.to_dict() for task in self.tasks] 
         }
 
-# Task model
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(500), nullable=False)
     due_date = db.Column(db.Date, nullable=False)
     status = db.Column(db.Enum(TaskStatus), nullable=False, default=TaskStatus.PENDING)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  
 
     def to_dict(self):
         return {
@@ -42,5 +56,5 @@ class Task(db.Model):
             "description": self.description,
             "due_date": self.due_date.isoformat(),
             "status": self.status.value,
-            "user_id": self.user_id
+            "user_id": self.user_id  
         }
