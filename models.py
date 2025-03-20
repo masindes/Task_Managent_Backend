6b9f07490207a0b62449f689f186f743d070edcf
Watty_Sylvester <88721196+masindes@users.noreply.github.com>
2025-03-20 14:13:51 +0300

    initail models

--- models.py
+++ models.py
@@ -20,8 +20,8 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     name = db.Column(db.String(120), nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)  # Hashed password
-    role = db.Column(db.Enum(UserRole), nullable=False, default=UserRole.USER)  # User role
+    password_hash = db.Column(db.String(128), nullable=False) 
+    role = db.Column(db.Enum(UserRole), nullable=False, default=UserRole.USER) 
     tasks = db.relationship('Task', backref='user', lazy=True)  
 
     def set_password(self, password):
