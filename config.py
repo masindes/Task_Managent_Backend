2f0cdf0f8c43438591b6b1d3bdcb9b75d0cad8af
Watty_Sylvester <88721196+masindes@users.noreply.github.com>
2025-03-20 17:43:20 +0300

    initial commit

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///task_management.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://task_manager_1p9m_user:TVSt6wBVEg3DDhe98JUOhPQs0qDPjC1W@dpg-cve1qjpc1ekc73ebk5f0-a.oregon-postgres.render.com/task_manager_1p9m')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'your-secret-key')
